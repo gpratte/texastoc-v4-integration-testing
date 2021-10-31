62194d78dc01cc1e1205ca4483bde63933cac7a4
Gil Pratte <gil.pratte@gmail.com>
2021-10-31 16:18:47 -0500

    log if chaos and multiple seasons settings

--- application/src/main/java/com/texastoc/config/IntegrationTestingConfig.java
+++ application/src/main/java/com/texastoc/config/IntegrationTestingConfig.java
@@ -1,10 +1,12 @@
 package com.texastoc.config;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Getter
+@Slf4j
 @Component
 public class IntegrationTestingConfig {
 
@@ -14,6 +16,8 @@ public class IntegrationTestingConfig {
   public IntegrationTestingConfig(
       @Value("${test.allowMultipleSeasons:false}") boolean allowMultipleSeasons,
       @Value("${test.allowChaos:false}") boolean allowChaos) {
+    log.info("allowMultipleSeasons: {}", allowMultipleSeasons);
+    log.info("allowChaos: {}", allowChaos);
     this.allowMultipleSeasons = allowMultipleSeasons;
     this.allowChaos = allowChaos;
   }
